b0322a6bc943f5632691f5c069d0a5215c64e451
Anatolii Ostapenko <anatoliy.ostapenko@gmail.com>
2022-03-25 11:02:54 +0200

    create Storyboarded protocol

---
+++ CoordinatorPattern_second/Storyboarded.swift
@@ -0,0 +1,20 @@
+//
+//  Storyboarded.swift
+//  CoordinatorPattern_second
+//
+//  Created by AnatoliiOstapenko on 25.03.2022.
+//
+
+import Foundation
+import UIKit
+
+protocol Storyboarded {
+    static func instantiate() -> Self
+}
+extension Storyboarded where Self: UIViewController {
+    static func instantiate() -> Self {
+        let id = String(describing: self)
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        return storyboard.instantiateViewController(withIdentifier: id) as! Self
+    }
+}
